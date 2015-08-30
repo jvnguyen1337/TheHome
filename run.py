416803a93430842e0494bede018ae5d37d6996b5
Abay Bektursun <abektursun@gmail.com>
2015-08-29 22:03:25 -0500

    Added Contacts and working garage button

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask              import Flask, render_template, flash
+from flask              import Flask, render_template, flash, jsonify, request
 from flask_bootstrap    import Bootstrap
 from flask_appconfig    import AppConfig
 from flask_wtf          import Form, RecaptchaField
@@ -7,45 +7,6 @@ from wtforms            import TextField, HiddenField, ValidationError, RadioFie
                         BooleanField, SubmitField, IntegerField, FormField, validators
 from wtforms.validators import Required
 #from GPIO               import open_garage
-
-
-
-"""# straight from the wtforms docs:
-class TelephoneForm(Form):
-    country_code = IntegerField('Country Code', [validators.required()])
-    area_code = IntegerField('Area Code/Exchange', [validators.required()])
-    number = TextField('Number')
-
-
-class ExampleForm(Form):
-    field1 = TextField('First Field', description='This is field one.')
-    field2 = TextField('Second Field', description='This is field two.',
-                       validators=[Required()])
-    hidden_field = HiddenField('You cannot see this', description='Nope')
-    recaptcha = RecaptchaField('A sample recaptcha field')
-    radio_field = RadioField('This is a radio field', choices=[
-        ('head_radio', 'Head radio'),
-        ('radio_76fm', "Radio '76 FM"),
-        ('lips_106', 'Lips 106'),
-        ('wctr', 'WCTR'),
-    ])
-    checkbox_field = BooleanField('This is a checkbox',
-                                  description='Checkboxes can be tricky.')
-
-    # subforms
-    mobile_phone = FormField(TelephoneForm)
-
-    # you can change the label as well
-    office_phone = FormField(TelephoneForm, label='Your office phone')
-
-    ff = FileField('Sample upload')
-
-    submit_button = SubmitField('Submit Form')
-
-
-    def validate_hidden_field(form, field):
-        raise ValidationError('Always wrong')
-"""
 
 def create_app(configfile=None):
     app = Flask(__name__)
@@ -58,6 +19,13 @@ def create_app(configfile=None):
     app.config['SECRET_KEY'] = 'devkey'
     app.config['RECAPTCHA_PUBLIC_KEY'] = \
         '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+    @app.route('/garage')
+    def add_numbers():
+        ajax_request = request.args.get('ajax_request', 0, type=str)
+        print "wassup!\nwassup!\nwassup!\nwassup!\nwassup!\nwassup!\nwassup!\n"
+        #open_garage()
+        return jsonify(result=ajax_request)
 
     @app.route('/', methods=('GET', 'POST'))
     def index():
